0f808d27ca51da61efd2c936555e97efab786077
Isma <didouhismael@gmail.com>
2020-07-29 23:09:01 +0200

    Create store in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
-  return (<div>
-
-    </div>
-  );
+  return <Provider store={store} />;
 }
 
 export default App;
